1946a703b61e7dacfaa3a158bfd82684dbd068f9
Andres Sanche <sanche94@gmail.com>
2020-12-03 16:44:25 -0600

    app update

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ from sklearn.preprocessing import MinMaxScaler
 
 app = Flask(__name__)
 
-#@app.route('/')
-#def hello():
+@app.route('/')
+def hello():
     # get param from http://127.0.0.1:5000/?name=value
-    #name = request.args.get("name", "World")
-    #return f'Hello, {escape(name)}!'
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
 
 
 
