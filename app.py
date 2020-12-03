1e38f37dd28256d427fcc2ddcde427a8484089a3
Andres Sanche <sanche94@gmail.com>
2020-12-03 16:22:17 -0600

    app update

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
+from flask import Flask, escape, request
 import streamlit as st
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import MinMaxScaler
+
+
+app = Flask(__name__)
+
+#@app.route('/')
+#def hello():
+    # get param from http://127.0.0.1:5000/?name=value
+    #name = request.args.get("name", "World")
+    #return f'Hello, {escape(name)}!'
+
+
+
 
 
 TEAMS = ["Atl Goianiense","Atl Paranaense","Atlético Mineiro","Bahia","Botafogo (RJ)", "Bragantino", "Ceará",
